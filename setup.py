af70146317d5ebc50591b30c845f4ac0edbac49a
hansnans <hansnans@gmail.com>
2020-07-09 15:34:44 +0200

    paste error in setup.py fixed

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ setup(
     name='eg_pyms_lan',
     version='2.0',
     packages=['eg_pms2_lan'],
-long_description    author='Marios Christodoulou',
+    author='Marios Christodoulou',
     long_description=long_description,
     long_description_content_type='text/markdown',
     author_email='marios.christodoulou@fkie.fraunhofer.de',
